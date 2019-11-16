003edddc2f43f46ee1f334f16dbf0501bab9dfb2
Laurent Grangeau <laurent.grangeau@gmail.com>
2019-11-16 14:10:41 +0100

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+
+    mounted() {
+      axios
+        .get("https://2rh8plyqnj.execute-api.us-east-1.amazonaws.com/dev/hello")
+        .then(response => (this.msg = response.data.message));
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
